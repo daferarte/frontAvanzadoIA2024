60b148cabe696d4c8422c2756127de1ffc676e21
daferarte <daferarte@gmail.com>
2024-05-22 21:13:01 -0500

    formulario inicio sesion

--- src/users/SingIn.jsx
+++ src/users/SingIn.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 
 let SignIn = (props) => {
     let user = false;
+    const {register, handleSubmit} = useForm();
+    let onSubmit = (data) => {
+        console.log(data.email);
+    };
     return (
         <div>
             {
-                user ? 
-                <button>Cerrar sesion</button>
-                :
-                <button>Ingresar</button>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input type="email" name="email" {...register("email")} placeholder="Correo electronico"/>
+                    <input type="password" name="password" {...register("password")} placeholder="Contrasenia"/>
+                    <input type="submit" value="Enviar" />
+                </form>
             }
         </div>
     )
