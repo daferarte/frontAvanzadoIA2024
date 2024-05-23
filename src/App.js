89e63d7cd64e5cba44da5c6dfd43159b65d1659e
daferarte <daferarte@gmail.com>
2024-05-22 20:48:00 -0500

    use params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Outlet, redirect, useNavigate, Navigate, useParams } from 'react-router-dom';
 
 let Hello = () => {
   return <h1>Hola estudiantes</h1>
@@ -16,15 +16,32 @@ let NotImplemented = () => {
 }
 
 let UsersOutlet = () => {
+  let navigate = useNavigate();
+  let redirect=()=>{
+    navigate('/');
+  }
   return (
     <>
       <p>Hola desde Usuarios</p>
+      <button onClick={redirect}>Ir al home</button>
       <Outlet/>
     </>
   )
 }
 
+let UserEdit = () => {
+  let {id} = useParams(); //capturo parametro
+  let params = useParams();
+  console.log(params);
+  return (
+    <>
+      <h1>{id}</h1>
+    </>
+  )
+}
+
 function App() {
+  const isAuth = false;
   return (
     <BrowserRouter>
       <Routes>
@@ -32,8 +49,8 @@ function App() {
 
       
         <Route path='/user' element={<UsersOutlet/>}>
-          <Route path='add' element={<NotImplemented/>} />
-          <Route path='edit/:id' element={<NotImplemented/>} />
+          <Route path='add' element={ isAuth ? <Navigate to={'/'}/>:<NotImplemented/>} />
+          <Route path='edit/:id' element={<UserEdit/>} />
           <Route path='delete/:id' element={<NotImplemented/>} />
         </Route>
 
